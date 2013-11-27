3827f884abe1801534f842985c1d87daf6f3d16b
Michał Motyczko <michal@motyczko.pl>
2013-11-27 21:13:39 +0100

    initial commit

---
+++ theme/src/main/java/pl/motyczko/dialer/theme/kitkat/Theme.java
@@ -0,0 +1,10 @@
+package pl.motyczko.dialer.theme.kitkat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Theme extends BroadcastReceiver {
+    @Override public void onReceive (Context context, Intent intent) {
+    }
+}
